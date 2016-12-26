c69d583d786a988003d24f4dae16bb28a5614764
Sam Smith <sam@example.com>
2016-12-26 20:46:26 +0530

    hello

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var path = require ('path');
 
 //app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname + '/public')));
-//app.get('*',function(req,res){
-//    res.send('index.html');
-//})
+app.get('/',function(req,res){
+    res.send({name:"Vishnu"});
+})
 var port = process.env.port || 52078;
 app.listen(port, function () {
     console.log('Example app listening on port' + port);
